08a2698187c4a4bb27a432eedb815e76cc4c62f9
vmunson <virginia.munson@gmail.com>
2018-01-16 16:21:01 -0500

    end of module 02

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    console.log('ready!')
+    $('#friend-form').submit((e) => {
+        e.preventDefault()
+        let newFriend = $('#input').val()
+        let newElement = createFriend(newFriend)
+        $('#friend-list').append(newFriend)
+        $('#input').val(' ')
+    })
 })
+
+function createFriend(name){
+    return $(`<li>${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark')
+}
